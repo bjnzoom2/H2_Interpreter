c7f704da563b936a8f486abd6c00decd40c88c9f
bjnzoom2 <bjnzoom2@gmail.com>
2025-12-20 20:48:37 +0800

    Made the return keyword print the returned value

--- generation.h
+++ generation.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <sstream>
 #include "parsing.h"
 
 class Generator {
@@ -8,8 +9,14 @@ public:
     explicit inline Generator(node::RetNode tree) : m_tree(std::move(tree)) {}
     [[nodiscard]] std::string generate() const {
         std::stringstream output;
-        output << "global main\nsection .text\nmain:\n";
-        output << "    mov eax, " << std::stoi(m_tree.expr.num_lit.text) << "\n";
+        output << "global main\nextern printf\nsection .data\nfmt db \"%d\", 10, 0\nsection .text\nmain:\n";
+        output << "    sub rsp, 32\n";
+        output << "    lea rcx, [rel fmt]\n";
+        output << "    mov edx, " << std::stoi(m_tree.expr.num_lit.text) << "\n";
+        output << "    xor eax, eax\n";
+        output << "    call printf\n";
+        output << "    add rsp, 32\n";
+        output << "    xor eax, eax\n";
         output << "    ret\n";
 
         return output.str();
